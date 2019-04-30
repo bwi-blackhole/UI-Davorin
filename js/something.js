18c703e59bf7139f5f88f93119e5445e158499af
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-04-29 21:09:47 -0600

    some work on carousel attempt, log in button styling added

--- js/something.js
+++ js/something.js
@@ -1,6 +1,10 @@
 class Carousel {
     constructor(element){
+   
         this.element = element
+        
+        this.box = this.element.querySelector('.something-box')
+
         this.rBtn = element.querySelector('.right-button')
         this.lBtn = element.querySelector('.left-button')
         this.img = element.querySelector('.carousel-img')
@@ -12,10 +16,18 @@ class Carousel {
         this.lBtn.addEventListener('click', () => {
             this.someFunc()
         })
-
+        
+        
+      }
         someFunc() {
+            let i = 0
+            this.box.classList.toggle('something-hidden')
+            alert(i)
+            return i++
             
         }
     }
 
-}
+
+
+let rows = document.querySelectorAll('.rows').forEach(element => new Carousel(element))
